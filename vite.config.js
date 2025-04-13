f05506d40e292403158cec7a955924a3c65777ce
Daniel Laumer <daniel.laumer@gmail.com>
2025-04-14 00:16:35 +0200

    Several branches deployed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/alpenchallenge/", // IMPORTANT: Set this to your repo name
+  base: process.env.VITE_BASE_PATH || '/', // Defaults to '/' if not set
   build: {
     outDir: "dist",
     emptyOutDir: true,
