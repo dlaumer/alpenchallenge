0024e3022aa29cdccfe5ee8cdadb72a4c16d3c23
Daniel Laumer <daniel.laumer@gmail.com>
2025-04-14 01:05:40 +0200

    Small bug

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/', // Defaults to '/' if not set
+  base: process.env.VITE_BASE_PATH || '/alpenchallenge/',
   build: {
     outDir: "dist",
     emptyOutDir: true,
