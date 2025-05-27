ed92a6a3cf6c4aa179b372b298e1c2719457438e
Christian Sailer <c.sailer@gmx.ch>
2025-05-27 15:23:46 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -227,7 +227,7 @@ const Header = observer(() => {
             </ProgressBarWrapper>
           </LiveTextWrapper>
         </LiveTitleWrapper>
-        3RIDES - Gran Fondo2
+        3RIDES - Gran Fondo
       </LeftSection>
 
       <RightSection>
